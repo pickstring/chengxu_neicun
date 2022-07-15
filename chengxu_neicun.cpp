b16ebca81467c6608941d9a1357eaa6a66f15ed0
FL <FL@DESKTOP-08Q858E>
2022-07-15 10:31:15 +0800

    内存

---
+++ chengxu_neicun.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main() {
+	/// <summary>
+	/// 1 代码区  函数体的二进制代码
+	/// 2 全局区  全局变量 静态变量 常量
+	/// 3 栈区  由编译器自动分配释放 函数的参数值 局部变量
+	/// 4 堆区  由程序员分配和释放 结束时由os回收
+	/// 生命周期 灵活管理
+	/// </summary>
+	/// <returns></returns>
+	return 0;
+}
